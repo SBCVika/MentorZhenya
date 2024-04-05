e156f59ad8fc3892ba6c41c1b4a883a620e64032
Yevhen Lytvyn <yevhen.lytvyn@volvocars.com>
2024-04-05 12:46:48 +0300

    MINOR: some fixes

--- Day2_Filereading.py
+++ Day2_Filereading.py
@@ -1,4 +1,5 @@
 import re
+import urllib.parse
 from time import perf_counter
 #1 . написати функцію яка відкриває текстовий файл і повертає дані )
 #2. написати функцію яка вимірює час виконання функції з пункту (1.) вивести час та імя функції
@@ -9,41 +10,51 @@ from time import perf_counter
 #1.0 < 1.0.1
 #функція має так сигнатуру def compare_versions(ver1, ver2):
 #подумай над тим що має бути результатом
+"2.45.2".split('.')
+import re
+p = re.compile('[\.\s]')
+re.split(p, "2.3.4", )
+
 
 def compare_versions(ver1, ver2):
     regex = re.compile(r'\d+')
-    ver1_num = [int(num) for num in re.findall(regex, ver1)]
-    ver2_num = [int(num) for num in re.findall(regex, ver2)]
-
+    ver1_num = [int(num) for num in re.finditer(regex, ver1)]
+    ver2_num = [int(num) for num in re.finditer(regex, ver2)]
+    lv1, lv2 = len(ver1_num), len(ver2_num)
     for i in range(min(len(ver1_num), len(ver2_num))):
         if ver1_num[i] > ver2_num[i]:
-            return "new"
+            return 1
         elif ver1_num[i] < ver2_num[i]:
-            return "old"
+            return -1
 
     if len(ver1_num) > len(ver2_num):
-        return "new"
+        return 1
     elif len(ver1_num) < len(ver2_num):
-        return "old"
-    elif len(ver1_num) == len(ver2_num):
-        return "same"
-def Readfile(filename):
+        return -1
+    else:
+        return 0
 
+
+def Readfile(filename):
     file = open('versions.txt', 'r')
     versions = file.readlines()  # I use readline instead of read because I would like to use output like a list
     file.close()
     return versions
-t1_start = perf_counter()
-versions = Readfile('versions.txt')
-t1_stop = perf_counter()
-print("Elapsed time:", t1_stop, t1_start)
-print(versions)
-ver1 = versions[-1]
-ver2 = versions[-2]
 
-if compare_versions(ver1, ver2) == "new":
-    print(f"Released new version {ver1}")
-elif compare_versions(ver1, ver2) == "old":
-    print("There are no new versions")
-elif compare_versions(ver1, ver2) == "same":
-    print("Same version is released")
+
+def measure_time(func, *args, **kwargs):
+    t1_start = perf_counter()
+    result = func(*args, **kwargs)
+    t1_stop = perf_counter()
+    print("Elapsed time:", t1_stop, t1_start)
+    return result
+
+
+measure_time(Readfile, ('versions.txt',))
+
+# if compare_versions(ver1, ver2) == "new":
+#     print(f"Released new version {ver1}")
+# elif compare_versions(ver1, ver2) == "old":
+#     print("There are no new versions")
+# elif compare_versions(ver1, ver2) == "same":
+#     print("Same version is released")

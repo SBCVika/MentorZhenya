913a4614c5a66340fff731ad9ae63ac20a6a75bd
Yevhen Lytvyn <yevhen.lytvyn@volvocars.com>
2024-04-29 13:24:45 +0300

    MINOR: added decorator

--- practice5_homework.py
+++ practice5_homework.py
@@ -6,51 +6,56 @@ from argparse import ArgumentParser
 class TooLargeContent(Exception):
     pass
 
+
+def except_file_errors(filename):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except FileNotFoundError as e:
+                print(f"Error: File '{filename}' not found. Please run writer first to create this file {e}")
+                raise e
+            except PermissionError as e:
+                print(f"Error: Permission denied for file '{filename}'. Please add permissions {e}")
+                raise e
+            except Exception as e:
+                print(f"Error: An unexpected error occurred while reading file '{filename}': {e}")
+                raise e
+        return wrapper
+    return decorator
+
+
 def get_reader(log_file):
     current_pos = 0
 
+    @except_file_errors(log_file)
     def read_next():
-        try:
-            with open(log_file, 'r') as file:
-                nonlocal current_pos
-                file.seek(current_pos, os.SEEK_SET)
-                content = file.read()
-                current_pos = file.tell()
-                return content
-        except FileNotFoundError as e:
-            print(f"Error: File '{log_file}' not found. Please run writer first to create this file {e}")
-            raise e
-        except PermissionError as e:
-            print(f"Error: Permission denied for file '{log_file}'. Please add permissions {e}")
-            raise e
-        except Exception as e:
-            print(f"Error: An unexpected error occurred while reading file '{log_file}': {e}")
-            raise e
+        with open(log_file, 'r') as file:
+            nonlocal current_pos
+            file.seek(current_pos, os.SEEK_SET)
+            content = file.read()
+            current_pos = file.tell()
+            return content
 
     return read_next
 
+
 def get_writer(output_file):
+    @except_file_errors(output_file)
     def write_data(data_to_write):
-        try:
-            with open(output_file, 'a') as file:
-                file.write(data_to_write)
-                print("Data written to", output_file)
-        except FileNotFoundError as e:
-            print(f"Error: File '{output_file}' not found. Please run the writer first to create this file. {e}")
-            raise e
-        except PermissionError as e:
-            print(f"Error: Permission denied for file '{output_file}'. Please add permissions. {e}")
-            raise e
-        except Exception as e:
-            print(f"Error: An unexpected error occurred while writing to file '{output_file}': {e}")
-            raise e
+        with open(output_file, 'a') as file:
+            file.write(data_to_write)
+            print("Data written to", output_file)
     return write_data
+
+
 def print_lines(content):
     if len(content) > 1 << 32:
         raise TooLargeContent()
     lines = content.splitlines(keepends=True)
     for line in lines:
         print(line)
+
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="Reader/Writer")
